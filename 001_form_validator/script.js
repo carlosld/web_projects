c37b6e720a8a6c6a3745443f5f50ae268d6d0c65
Carlos León <cleondorta@gmail.com>
2020-08-02 16:29:21 +0100

    Add more validations

--- 001_form_validator/script.js
+++ 001_form_validator/script.js
@@ -4,8 +4,8 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 
-// Show input ereor message
-function showError(input, message) {
+// Show input error message
+function showError (input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
 
@@ -14,13 +14,23 @@ function showError(input, message) {
 }
 
 // Show success outline
-function showSuccess(input) {
+function showSuccess (input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
+// Check email is valid
+function checkEmail (input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
 // Require validation
-function requireValidation(input, fieldName) {
+function requireValidation (input, fieldName) {
   if (input.value === '') {
     showError(input, `${fieldName} is required`);
   } else {
@@ -28,12 +38,47 @@ function requireValidation(input, fieldName) {
   }
 }
 
+// Check required fields
+function checkRequired (inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check input length
+function checkLength (input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be at less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check password match 
+function checkPasswordsMatch (input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+// Get field name
+function getFieldName (input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  requireValidation(username, 'User name');
-  requireValidation(email, 'Email');
-  requireValidation(password, 'Password');
-  requireValidation(confirmpassword, 'Confirm password');
+  checkRequired([username, email, password, confirmPassword]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, confirmPassword);
 });
